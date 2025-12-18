ed03a0695104586539cb964e2aa450a157bd18d1
alpharaoh <alpharaohh@gmail.com>
2025-12-18 21:17:00 +0000

    Type the inngest event

--- web/services/api/src/inngest/client.ts
+++ web/services/api/src/inngest/client.ts
@@ -1,5 +1,9 @@
-import { Inngest } from "inngest";
+import { EventSchemas, Inngest } from "inngest";
+import { TestEvent } from "./functions/test";
+
+type Events = TestEvent;
 
 export const inngest = new Inngest({
   id: "zanin.ai",
+  schemas: new EventSchemas().fromRecord<Events>(),
 });
--- web/services/api/src/inngest/functions/test.ts
+++ web/services/api/src/inngest/functions/test.ts
@@ -1,8 +1,19 @@
 import { inngest } from "../client";
 
+type UserSignup = {
+  data: {
+    email: string;
+    name: string;
+  };
+};
+
+export type TestEvent = {
+  "user/new.signup": UserSignup;
+};
+
 export default inngest.createFunction(
   { id: "import-product-images" },
-  { event: "shop/product.imported" },
+  { event: "user/new.signup" },
   async ({ event, step }) => {
     console.log("Function called", event);
   },
