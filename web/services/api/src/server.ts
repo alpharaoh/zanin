0932e6da7ce06276185b7773924d119aafb30759
alpharaoh <alpharaohh@gmail.com>
2025-12-18 04:10:38 +0000

    Fix loggign

--- web/services/api/src/server.ts
+++ web/services/api/src/server.ts
@@ -1,4 +1,31 @@
-import { app } from "./app";
+import cors from "cors";
+import express, { json, urlencoded, Response, Request } from "express";
+import { RegisterRoutes } from "../build/routes";
+import swaggerUi from "swagger-ui-express";
+import { errorHandler } from "./handlers/errorHandler";
+import { notFoundHandler } from "./handlers/notFoundHandler";
+import pino from "pino-http";
+
+export const app = express();
+
+app.use(pino());
+app.use(cors());
+app.use(
+  urlencoded({
+    extended: true,
+  }),
+);
+app.use(json());
+app.use("/docs", swaggerUi.serve, async (_: Request, res: Response) => {
+  return res.send(
+    swaggerUi.generateHTML(await import("../build/swagger.json")),
+  );
+});
+
+RegisterRoutes(app);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 const port = process.env.PORT || 8081;
 
