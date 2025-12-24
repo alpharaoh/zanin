c0a07c88dbc214d31b2b229d87a858700dc51ae6
alpharaoh <alpharaohh@gmail.com>
2025-12-24 18:35:02 +0000

    Init first AI agent

---
+++ web/services/api/src/agents/recordings/nodes/llmNode.ts
@@ -0,0 +1,17 @@
+import { SystemMessage } from "@langchain/core/messages";
+import { RecordingAgentStateType } from "../state";
+import { modelWithTools } from "../tools";
+
+export async function llmCall(state: RecordingAgentStateType) {
+  return {
+    messages: [
+      await modelWithTools.invoke([
+        new SystemMessage(
+          "You are a helpful assistant tasked with performing arithmetic on a set of inputs.",
+        ),
+        ...state.messages,
+      ]),
+    ],
+    llmCalls: 1,
+  };
+}
