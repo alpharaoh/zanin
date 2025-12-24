734a5f29b34585dbf72861d03a1fb4586f73cddb
alpharaoh <alpharaohh@gmail.com>
2025-12-24 01:23:36 +0000

    Started to make LED

--- firmware/main/zanin-firmware.cpp
+++ firmware/main/zanin-firmware.cpp
@@ -7,27 +7,56 @@
 #include "freertos/task.h"
 #include "led_strip.h"
 
+// The GPIO that connected to the LED
 const unsigned int GPIO_RGB_LED_ID = 38;
 
-extern "C" void app_main(void) {
-  char *tag = pcTaskGetName(NULL);
-  ESP_LOGI(tag, " -> Hello from ESP32-S3!\n");
+// The number of LEDs in the strip
+const unsigned int LED_STRIP_LED_COUNT = 1;
 
+// 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
+const unsigned int LED_STRIP_RMT_RES_HZ = (10 * 1000 * 1000);
+
+// Set to 1 to use DMA for driving the LED strip, 0 otherwise
+const unsigned int LED_STRIP_USE_DMA = 0;
+
+// let the driver choose a proper memory block size automatically
+const unsigned int LED_STRIP_MEMORY_BLOCK_WORDS = 0;
+
+static const char *TAG = "example";
+
+led_strip_handle_t configure_led(void) {
   /// LED strip common configuration
   led_strip_config_t strip_config = {
-      .strip_gpio_num = GPIO_RGB_LED_ID, // The GPIO that connected to the LED
-                                         // strip's data line
-      .max_leds = 1,                     // The number of LEDs in the strip,
+      .strip_gpio_num = GPIO_RGB_LED_ID,
+      .max_leds = LED_STRIP_LED_COUNT,
       .led_model =
           LED_MODEL_WS2812, // LED strip model, it determines the bit timing
-      .color_component_format =
-          LED_STRIP_COLOR_COMPONENT_FMT_GRB, // The color component format is
-                                             // G-R-B
+      .color_component_format = LED_STRIP_COLOR_COMPONENT_FMT_GRB, // G-R-B
       .flags = {
-          .invert_out = false, // don't invert the output signal
+          .invert_out = false, // Don't invert the output signal
       }};
 
-  led_strip_handle_t led_strip = NULL;
+  // LED strip backend configuration: RMT
+  led_strip_rmt_config_t rmt_config = {
+      .clk_src = RMT_CLK_SRC_DEFAULT,
+      .resolution_hz = LED_STRIP_RMT_RES_HZ,
+      .mem_block_symbols =
+          LED_STRIP_MEMORY_BLOCK_WORDS, // the memory block size used by the RMT
+                                        // channel
+      .flags = {
+          .with_dma = LED_STRIP_USE_DMA, // Using DMA can improve performance
+                                         // when driving more LEDs
+      }};
+
+  // LED Strip object handle
+  led_strip_handle_t led_strip;
   ESP_ERROR_CHECK(
-      led_strip_new_spi_device(&strip_config, &spi_config, &led_strip));
+      led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+  ESP_LOGI(TAG, "Created LED strip object with RMT backend");
+  return led_strip;
+}
+
+extern "C" void app_main() {
+  char *tag = pcTaskGetName(NULL);
+  ESP_LOGI(tag, " -> Hello from ESP32-S3!\n");
 }
