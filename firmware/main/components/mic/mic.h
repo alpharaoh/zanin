d866ba2da0431eaaf8c2dcf4755e05de3c4db43f
alpharaoh <alpharaohh@gmail.com>
2025-12-25 18:16:19 +0000

    Progress on microphone

--- firmware/main/components/mic/mic.h
+++ firmware/main/components/mic/mic.h
@@ -1,13 +1,18 @@
+#include "driver/i2s_types.h"
+#include "esp_err.h"
+#include "soc/gpio_num.h"
+
 class Microphone {
 public:
-  Microphone(int blckGpioId, int blckDioGpioId, int blckLrclGpioId);
-
-  const unsigned int MIC_BLCK_GPIO_ID = 17;
-  const unsigned int MIC_DOUT_GPIO_ID = 16;
-  const unsigned int MIC_LRCL_GPIO_ID = 15;
+  Microphone(gpio_num_t blck, gpio_num_t dio, gpio_num_t lrcl);
+  void start();
+  void stop();
+  esp_err_t read(void *buffer, size_t bytes_to_read, size_t *bytes_read,
+                 uint32_t timeout_ms);
 
 private:
-  int blckGpioId;
-  int blckDioGpioId;
-  int blckLrclGpioId;
+  gpio_num_t blck;
+  gpio_num_t dio;
+  gpio_num_t lrcl;
+  i2s_chan_handle_t rx_handle;
 };
--- firmware/main/zanin-firmware.cpp
+++ firmware/main/zanin-firmware.cpp
@@ -10,17 +10,20 @@
 #include "freertos/task.h"
 #include "led_strip.h"
 
-// The GPIO that connected to the LED
-const unsigned int LED_GPIO_ID = 38;
+const gpio_num_t LED_GPIO_ID = GPIO_NUM_38;
 
-const unsigned int MIC_BLCK_GPIO_ID = 17;
-const unsigned int MIC_DOUT_GPIO_ID = 16;
-const unsigned int MIC_LRCL_GPIO_ID = 15;
+const gpio_num_t MIC_BLCK_GPIO_ID = GPIO_NUM_17;
+const gpio_num_t MIC_DOUT_GPIO_ID = GPIO_NUM_16;
+const gpio_num_t MIC_LRCL_GPIO_ID = GPIO_NUM_15;
 
 extern "C" void app_main() {
   Led led = Led(LED_GPIO_ID);
   Microphone mic =
       Microphone(MIC_BLCK_GPIO_ID, MIC_DOUT_GPIO_ID, MIC_LRCL_GPIO_ID);
+
+  mic.start();
+  ESP_LOGI("zanin-mic", "Mic started");
+  mic.stop();
 
   while (true) {
     if (!led.isOn()) {
