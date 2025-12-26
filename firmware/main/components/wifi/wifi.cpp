6d0f1d6b6cbb0cc8793d542fdf3579e5a8fa94a9
alpharaoh <alpharaohh@gmail.com>
2025-12-26 22:12:11 +0000

    init wifi

---
+++ firmware/main/components/wifi/wifi.cpp
@@ -0,0 +1,90 @@
+#include "wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/idf_additions.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+static const char *TAG = "zanin-wifi";
+
+constexpr const char *ESP_WIFI_SSID = "A's router";
+constexpr const char *ESP_WIFI_PASS = "kmzcnxLn59gd";
+constexpr const int EXAMPLE_ESP_MAXIMUM_RETRY = 4;
+
+/* The event group allows multiple bits for each event, but we only care about
+ * two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+constexpr const int WIFI_CONNECTED_BIT = BIT0;
+constexpr const int WIFI_FAIL_BIT = BIT1;
+
+Wifi::Wifi() {
+  s_wifi_event_group = xEventGroupCreate();
+
+  ESP_ERROR_CHECK(esp_netif_init());
+
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_create_default_wifi_sta();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  esp_event_handler_instance_t instance_any_id;
+  esp_event_handler_instance_t instance_got_ip;
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
+
+  wifi_config_t wifi_config = {
+      .sta =
+          {
+              .ssid = ESP_WIFI_SSID,
+              .password = ESP_WIFI_PASS,
+              /* Authmode threshold resets to WPA2 as default if password
+               * matches WPA2 standards (password len => 8). If you want to
+               * connect the device to deprecated WEP/WPA networks, Please set
+               * the threshold value to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK and set
+               * the password with length and format matching to
+               * WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK standards.
+               */
+              .threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,
+              .sae_pwe_h2e = ESP_WIFI_SAE_MODE,
+              .sae_h2e_identifier = EXAMPLE_H2E_IDENTIFIER,
+#ifdef CONFIG_ESP_WIFI_WPA3_COMPATIBLE_SUPPORT
+              .disable_wpa3_compatible_mode = 0,
+#endif
+          },
+  };
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+
+  ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+  /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or
+   * connection failed for the maximum number of re-tries (WIFI_FAIL_BIT). The
+   * bits are set by event_handler() (see above) */
+  EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                         WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                         pdFALSE, pdFALSE, portMAX_DELAY);
+
+  /* xEventGroupWaitBits() returns the bits before the call returned, hence we
+   * can test which event actually happened. */
+  if (bits & WIFI_CONNECTED_BIT) {
+    ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", ESP_WIFI_SSID,
+             ESP_WIFI_PASS);
+  } else if (bits & WIFI_FAIL_BIT) {
+    ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", ESP_WIFI_SSID,
+             ESP_WIFI_PASS);
+  } else {
+    ESP_LOGE(TAG, "UNEXPECTED EVENT");
+  }
+}
---
+++ firmware/main/components/wifi/wifi.h
@@ -0,0 +1,6 @@
+class Wifi {
+public:
+  Wifi();
+
+private:
+};
